8f600d61b61f0d1c813cc4239ec6bfd2c267f60c
podum <patrick.odum1@gmail.com>
2021-04-23 01:44:48 +0000

    added ngx-translate+

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,30 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule, // <--- add this
+    TranslateModule.forRoot({ // <--- add this
+      loader: { // <--- add this
+        provide: TranslateLoader, // <--- add this
+        useFactory: (createTranslateLoader),  // <--- add this
+        deps: [HttpClient] // <--- add this
+      } // <--- add this
+    }) // <--- add this
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
